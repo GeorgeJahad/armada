e42efa6010d39667014ffcdd7ad1c3b48e4e0cc4
George Jahad <george@georgejahad.com>
2022-04-20 20:57:13 -0700

    cleanup

--- pkg/api/gbj.py
+++ pkg/api/gbj.py
@@ -6,10 +6,15 @@ import event_pb2_grpc
 import sys
 from pprint import pprint
 
+startMessage = "0"
+if len(sys.argv) > 1:
+    startMessage = sys.argv[1]
+    
 def run():
+    print("checking queue-a, job-set-1")
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = event_pb2_grpc.EventStub(channel)
-        response2 = stub.GetJobSetEvents(event_pb2.JobSetRequest(queue="queue-a", from_message_id = sys.argv[1], watch=True, id ="job-set-1", errorIfMissing=True))
+        response2 = stub.GetJobSetEvents(event_pb2.JobSetRequest(queue="queue-a", from_message_id = startMessage, watch=True, id ="job-set-1", errorIfMissing=True))
         for resp in response2:
            print("client received: " + resp.id + " " + resp.message.WhichOneof("events"))
 
