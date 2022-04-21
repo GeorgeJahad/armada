7b0a9ccbf025ff50e33351d866d2eba985dba142
George Jahad <george@georgejahad.com>
2022-04-20 20:08:58 -0700

    next

--- pkg/api/gbj.py
+++ pkg/api/gbj.py
@@ -5,7 +5,7 @@ import logging
 import grpc
 import submit_pb2
 import submit_pb2_grpc
-
+import sys
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
@@ -15,8 +15,10 @@ def run():
         stub = submit_pb2_grpc.SubmitStub(channel)
         response = stub.CreateQueue(submit_pb2.Queue(name='gbj13', priority_factor=2))
 #        response2 = stub.CancelJobs(submit_pb2.JobCancelRequest(queue="queue-a", job_set_id ="job-set-1", job_id = "jj"))
-        response2 = stub.CancelJobs(submit_pb2.JobCancelRequest(queue="queue-a", job_set_id ="job-set-1"))
+        response2 = stub.CancelJobs(submit_pb2.JobCancelRequest(queue="queue-a", job_set_id ="job-set-1", job_id = sys.argv[1]))
     print("gbj client received: " + str(len(response2.cancelled_ids)))
+    if len(response2.cancelled_ids) > 0:
+        print("gbj cancelled id: " + str(response2.cancelled_ids[0]))
 
 
 if __name__ == '__main__':
